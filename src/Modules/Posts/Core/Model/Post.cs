2f80ede1569f1a2f96617669b04071476744395e
Dominik.Kotecki <dominikus1910@gmail.com>
2022-03-05 18:42:10 +0000

    small changes

--- src/Modules/Posts/Core/Model/Post.cs
+++ src/Modules/Posts/Core/Model/Post.cs
@@ -1,4 +1,5 @@
+using System.Collections.Generic;
 namespace DevMikroblog.Modules.Posts.Core.Model;
 
 public readonly record struct PostId(Guid Value);
-public record Post(PostId Id, string Content);
+public record Post(PostId Id, string Content, IList<Post> Replies, DateTime CreatedAt);
--- src/Modules/Posts/Infrastructure/Model/EfPost.cs
+++ src/Modules/Posts/Infrastructure/Model/EfPost.cs
@@ -2,5 +2,7 @@ namespace DevMikroblog.Modules.Posts.Infrastructure.Model;
 
 class EfPost 
 {
+    public Guid Id { get; set; }
+    public string Content { get; set; }
     
 }
