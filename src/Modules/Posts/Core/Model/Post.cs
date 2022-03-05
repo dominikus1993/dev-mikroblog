cd6b483735ded16905f029bd7c7e36fc2afb4621
Dominik.Kotecki <dominikus1910@gmail.com>
2022-03-05 18:45:20 +0000

    .

--- src/Modules/Posts/Core/Model/Post.cs
+++ src/Modules/Posts/Core/Model/Post.cs
@@ -2,4 +2,4 @@ using System.Collections.Generic;
 namespace DevMikroblog.Modules.Posts.Core.Model;
 
 public readonly record struct PostId(Guid Value);
-public record Post(PostId Id, string Content, IList<Post> Replies, DateTime CreatedAt);
+public record Post(PostId Id, string Content, IList<Post> Replies, DateTime CreatedAt, int Linkes);
