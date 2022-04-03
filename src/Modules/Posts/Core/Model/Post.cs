c8394e70650d4b5f584d6a81c3ba5383b959785a
Dominik.Kotecki <dominikus1910@gmail.com>
2022-04-03 08:29:14 +0200

    add unit test project

--- src/Modules/Posts/Core/Model/Post.cs
+++ src/Modules/Posts/Core/Model/Post.cs
@@ -1,8 +1,7 @@
-using System.Collections.Generic;
 using StronglyTypedIds;
 
 namespace DevMikroblog.Modules.Posts.Core.Model;
 
 [StronglyTypedId(converters: StronglyTypedIdConverter.SystemTextJson)]
 public partial struct PostId{}
-public record Post(PostId Id, string Content, IList<Post> Replies, DateTime CreatedAt, int Linkes);
+public record Post(PostId Id, string Content, IList<Post> Replies, DateTime CreatedAt, int Likes);
---
+++ tests/Modules/Posts.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Posts.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
